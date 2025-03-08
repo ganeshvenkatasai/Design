bd5989a4f6ba4142583f1311891c8705e0b63301
ganeshvenkatasai <ganeshmanchi123@gmail.com>
2025-03-09 02:20:59 +0530

    Add Behavioural Design Patterns

---
+++ design-patterns/behavioral/chain-of-responsibility/Main.java
@@ -0,0 +1,57 @@
+
+
+abstract class LeaveHandler {
+    protected LeaveHandler nextHandler;
+
+    abstract void handleRequest(int days);
+
+    void setNextHandler(LeaveHandler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+}
+
+class TeamLead extends LeaveHandler{
+    @Override
+    void handleRequest(int days) {
+        if (days < 3) {
+            System.out.println("Team Lead approved " + days + " days Leave");;
+        } else {
+            nextHandler.handleRequest(days);
+        }
+    }
+}
+
+class Manager extends LeaveHandler{
+    @Override
+    void handleRequest(int days) {
+        if (days < 8) {
+            System.out.println("Manager approved " + days + " days Leave");;
+        } else {
+            nextHandler.handleRequest(days);
+        }
+    }
+}
+
+class CEO extends LeaveHandler{
+    @Override
+    void handleRequest(int days) {
+        System.out.println("CEO approved " + days + " days Leave");;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        LeaveHandler teamLead = new TeamLead();
+        LeaveHandler manager = new Manager();
+        LeaveHandler ceo = new CEO();
+
+        teamLead.setNextHandler(manager);
+        manager.setNextHandler(ceo);
+
+        teamLead.handleRequest(2);
+        teamLead.handleRequest(7);
+        teamLead.handleRequest(30);
+
+    }
+}
